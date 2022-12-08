df4b51df9b76d0828c91483bbd56ed93637c69fb
EmreCanBerktas <73836132+EmreCanBerktas@users.noreply.github.com>
2022-12-08 16:55:28 +0300

    Add: circle color is now random

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import "./app.css";
 function App() {
   const popped = useRef([]);
   const [points, setPoints] = useState([]);
+  const [color, setColor] = useState("blue");
   const handlePlaceCircle = (e) => {
     const { clientX, clientY } = e;
-    setPoints([...points, { x: clientX, y: clientY }]);
+    setPoints([...points, { x: clientX, y: clientY, color }]);
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setColor(randomColor);
   };
+
   const handleUndo = () => {
     const newPoints = [...points];
     const poppedPoint = newPoints.pop();
@@ -20,9 +24,11 @@ function App() {
     setPoints([...points, poppedPoint]);
     popped.current = newPopped;
   };
+
   const current = popped.current;
   console.log({ points });
   console.log({ current });
+  console.log({ color });
   return (
     <>
       <button onClick={handleUndo} disabled={points.length >= 1 ? false : true}>
@@ -34,12 +40,22 @@ function App() {
       >
         Redo
       </button>
+
       <div className="App" onClick={handlePlaceCircle}>
         {points.map((point, i) => (
           <div
             className="point"
             key={i}
-            style={{ left: point.x - 5 + "px", top: point.y - 5 + "px" }}
+            style={{
+              left: point.x - 5 + "px",
+              top: point.y - 5 + "px",
+              display: "inline-block",
+              position: "absolute",
+              width: "15px",
+              height: "15px",
+              borderRadius: "50%",
+              backgroundColor: color,
+            }}
           ></div>
         ))}
       </div>
